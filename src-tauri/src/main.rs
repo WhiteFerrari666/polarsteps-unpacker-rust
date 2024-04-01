b12f546df4d2226931ecff97c6ad211fabbf9e7e
Martin <der_moaddin@yahoo.de>
2024-04-01 16:39:45 +0200

    fix dummy message in main.rs

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -4,7 +4,7 @@
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
 fn greet(name: &str) -> String {
-    format!("Hello! Your file is being generated in Rust!", name)
+    format!("Hello! Your data at {} is being made into a file, generated in Rust!", name)
 }
 
 fn main() {
